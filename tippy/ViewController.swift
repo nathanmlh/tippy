9d759a594c72436f4314238a5ce2b92a77d3663e
nathanmlh <nathanholdom1@gmail.coomo>
2021-01-02 17:34:59 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Nathan Holdom on 1/2/21.
+//  Copyright © 2021 Nathan Holdom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
